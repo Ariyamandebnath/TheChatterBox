f242d6071e5e0b036cb99e97a880268a0949ded3
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-03-23 11:41:15 +0530

    cleaner a version

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,24 @@ dotenv.config({
     path: './.env',
 });
 
-connectDB()
-    .then(() => {
-        httpServer.listen(process.env.PORT || 3000, () => {
-            console.log(`Server is running at : ${process.env.PORT}`);
-        });
-        httpServer.on("error", (err: NodeJS.ErrnoException) => { // Assuming httpServer is of type http.Server
-            console.error("Server error: ", err);
-        });
-    })
-    .catch((err: Error) => {
-        console.error(`MongoDB connection failed: ${err.message}`);
+
+const startServer = () => {
+    httpServer.listen(process.env.PORT || 3000, () => {
+        console.info(
+            ` Visit cocumentation at : http://localhost:${process.env.PORT || 3000
+            }`
+        );
+        console.log("⚙️  Server is running on port: " + process.env.PORT);
     });
+};
+
+const start = async () => {
+    try {
+        await connectDB();
+        startServer();
+    } catch (err) {
+        console.log("Mongo db connect error: ", err)
+    }
+};
+
+start();
