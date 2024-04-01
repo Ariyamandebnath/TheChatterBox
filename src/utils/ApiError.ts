0f521b170948fedf09d2abe0f8212d213e940d6f
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-04-01 23:49:18 +0530

    fixed error handeler

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,31 +1,31 @@
-import {errorHandler} from "../middlewares/error.middleware"
-
+import { errorHandler } from "../middlewares/error.middleware";
 
 class ApiError extends Error {
-    public readonly statusCode: number;
-    public readonly message: string;
-    public readonly errors: any[];
-    public readonly stack?: string;
-    public readonly success: boolean;
+  statusCode: number;
+  data: any | null;
+  message: string;
+  success: boolean;
+  errors: any[];
 
-    constructor(
-        statusCode: number,
-        message: string = "Something went wrong",
-        errors: any[] = [],
-        stack: string = ""
-    ) {
-        super(message);
-        this.statusCode = statusCode;
-        this.message = message;
-        this.errors = errors;
-        this.success = false;
+  constructor(
+    statusCode: number,
+    message = "Something went wrong",
+    errors: any[] = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
 
-        if (stack) {
-            this.stack = stack;
-        } else {
-            Error.captureStackTrace(this, this.constructor);
-        }
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
 export { ApiError };
