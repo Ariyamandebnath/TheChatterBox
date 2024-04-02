c3e3de71bbdf5093b8d40685998330be1fb291ef
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-04-02 20:42:00 +0530

    bug fixes

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,32 +1,34 @@
 import mongoose from "mongoose";
-import logger from "../logger/winston.logger"
-import { ApiError } from "utils/ApiError";
-import { asyncHandler } from "utils/asyncHandler";
-import { Request } from 'express';
+
+import logger from "../logger/winston.logger.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import express from 'express';
 
 
-//This middleware will catch the errors from any request handeler wrapped instide asyncHandler
 
-const errorHandler = (err: ApiError|Error, req: express.Request, res: express.Response, next:express.NextFunction) => {
-    let error = err;
+const errorHandler = (err :any, req :express.Request, res:express.Response, next:express.NextFunction) => {
+  let error = err;
 
-    //Check ;if the error is an instance of an ApiError class which extends native Error class
-    if (!(error instanceof ApiError)) {
+  // Check if the error is an instance of an ApiError class which extends native Error class
+  if (!(error instanceof ApiError)) {
+    // if not
+    // create a new ApiError instance to keep the consistency
 
-        const statusCode = error.statusCode || error instanceof mongoose.Error ? 400 : 500;
+    // assign an appropriate status code
+    const statusCode =
+      error.statusCode || error instanceof mongoose.Error ? 400 : 500;
 
-        const message = error.message || "Something went wrong";
-        error = new ApiError(statusCode, message, error?.errors || [], err.stack);
-    }
+    // set a message from native Error instance or a custom one
+    const message = error.message || "Something went wrong";
+    error = new ApiError(statusCode, message, error?.errors || [], err.stack);
+  }
 
-
-// Now we are sure that the `error` variable will be an instance of ApiError class
-const response = {
+  // Now we are sure that the `error` variable will be an instance of ApiError class
+  const response = {
     ...error,
     message: error.message,
-    ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}),
-    // Error stack traces should be visible in development for debugging
+    ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}), // Error stack traces should be visible in development for debugging
   };
 
   logger.error(`${error.message}`);
